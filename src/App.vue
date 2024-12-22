96c9a7df8cd6e41c6bfea85397b84af2e9ace2ea
sougo0910 <sougo.ks@gmail.com>
2024-12-23 00:37:02 +0900

    feat:v-bindの勉強

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import reactivePractice from './components/reactivePractice.vue'
+import directivePractice from './components/directivePractice.vue'
 </script>
 
 <template>
   <div className="div">
     <reactivePractice />
+    <hr />
+    <directivePractice />
     <hr />
   </div>
 </template>
---
+++ src/components/directivePractice.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import type { Ref } from 'vue'
+
+const message: Ref<string> = ref('<h1>Directive</h1>')
+const vBindURL: Ref<string> = ref('https://www.google.com/')
+const href: Ref<string> = ref('https://www.google.com/')
+const id: Ref<string> = ref('anchor')
+</script>
+
+<template>
+  <!-- Start: v-html -->
+  <div v-html="message"></div>
+  <!-- End: v-html -->
+
+  <!-- Start: v-bind -->
+  <a :href="vBindURL">v-bind link</a>
+  <a :id :href>v-bind link（省略）{{ id }}</a>
+  <button type="submit" :disabled="true">Button</button>
+  <!-- End: v-bind -->
+</template>
+
+<style scoped></style>
