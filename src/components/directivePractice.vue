ebd28991b05fb48ad61618f5f69eb3668eed34f4
komatsuzaki-sogo <komatsuzaki-sogo@mitsue.co.jp>
2024-12-23 17:27:30 +0900

    feat:computedの勉強

--- src/components/directivePractice.vue
+++ src/components/directivePractice.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import type { Ref } from 'vue'
+  import { ref } from 'vue'
+  import type { Ref } from 'vue'
 
-// v-html
-const vHtmlMessage: Ref<string> = ref('<h2>ディレクティブ:v-html・v-bind・v-onの勉強</h2>')
+  // v-html
+  const vHtmlMessage: Ref<string> = ref('<h2>ディレクティブ:v-html・v-bind・v-onの勉強</h2>')
 
-// v-bind
-const vBindURL: Ref<string> = ref('https://www.google.com/')
-const href: Ref<string> = ref('https://www.google.com/')
-const id: Ref<string> = ref('anchor')
+  // v-bind
+  const vBindURL: Ref<string> = ref('https://www.google.com/')
+  const href: Ref<string> = ref('https://www.google.com/')
+  const id: Ref<string> = ref('anchor')
 
-// v-on
-const eventName = 'click'
-const count: Ref<number> = ref(0)
-const handleClick = (event: MouseEvent, times: number) => {
-  count.value = event.clientX * times
+  // v-on
+  const eventName = 'click'
+  const count: Ref<number> = ref(0)
+  const handleClick = (event: MouseEvent, times: number) => {
+    count.value = event.clientX * times
 
-  if (count.value > 5) {
-    const target = event.currentTarget as HTMLElement
-    target.classList.add('bg-red')
+    if (count.value > 5) {
+      const target = event.currentTarget as HTMLElement
+      target.classList.add('bg-red')
+    }
   }
-}
 </script>
 
 <template>
@@ -28,10 +28,14 @@ const handleClick = (event: MouseEvent, times: number) => {
   <div v-html="vHtmlMessage"></div>
   <!-- End: v-html -->
 
+  <h3>v-bind</h3>
+
   <!-- Start: v-bind -->
   <a :href="vBindURL">v-bind link</a>
   <a :id :href>v-bind link（省略）{{ id }}</a>
   <!-- End: v-bind -->
+
+  <h3>v-on</h3>
 
   <!-- Start: v-on -->
   <div>{{ count }}</div>
@@ -49,7 +53,7 @@ const handleClick = (event: MouseEvent, times: number) => {
 </template>
 
 <style scoped>
-.bg-red {
-  background-color: red;
-}
+  .bg-red {
+    background-color: red;
+  }
 </style>
--- src/components/directivePracticeType2.vue
+++ src/components/directivePracticeType2.vue
@@ -1,16 +1,45 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
   import type { Ref } from 'vue'
   const inputRef: Ref<string> = ref('')
+
+  const score: Ref<number> = ref(0)
+  const handleClick = () => {
+    score.value++
+  }
+
+  const computedScore = computed<string>(() => {
+    return score.value > 3 ? 'Good' : 'Bad'
+  })
+
+  const getScore = () => {
+    console.log(computedScore.value)
+  }
+
+  const resetScore = () => {
+    score.value = 0
+  }
 </script>
 
 <template>
   <h2>ディレクティブ:v-model・computed・watcherの勉強</h2>
 
+  <h3>v-model</h3>
+
   <!-- Start: v-model -->
   <div>text:{{ inputRef }}</div>
   <input v-model="inputRef" type="text" />
   <!-- End: v-model -->
+
+  <h3>computed</h3>
+
+  <!-- Start: computed -->
+  <div>Score:{{ score }}</div>
+  <div>{{ computedScore }}</div>
+  <button type="button" @click="handleClick">+1</button>
+  <button type="button" @click="getScore">getScore</button>
+  <button type="button" @click="resetScore">resetScore</button>
+  <!-- End: computed -->
 </template>
 
 <style scoped></style>
