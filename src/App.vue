4cac5c2a1410ce815643b80834260213a7df058e
sougo0910 <sougo.ks@gmail.com>
2024-12-25 19:05:16 +0900

    fix:definePropsの勉強

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import type { Ref } from 'vue'
+  import DefinePropsPractice from './components/DefinePropsPractice.vue'
+  const totalCount: Ref<number> = ref(0)
+</script>
 
 <template>
   <h1>Vue Practice</h1>
+  <DefinePropsPractice :total-count="totalCount" />
+  <button type="button" @click="totalCount++">+1</button>
 </template>
 
 <style scoped>
---
+++ src/components/DefinePropsPractice.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  import { computed, watch } from 'vue'
+
+  const { totalCount, totalCount2 } = defineProps({
+    totalCount: {
+      type: Number,
+      required: true,
+    },
+    totalCount2: {
+      type: [Object, Number, String],
+      default: () => ({ age: 10, age2: 20 }),
+    },
+  })
+
+  watch(
+    () => totalCount,
+    (newValue, oldValue) => {
+      console.log('watch')
+      console.log(newValue)
+      console.log(oldValue)
+    },
+    {
+      immediate: true,
+    },
+  )
+
+  const computedTotalCount = computed(() => {
+    return totalCount > 5 ? 'Good' : 'Bad'
+  })
+</script>
+
+<template>
+  <div>totalCount: {{ totalCount }}</div>
+  <div>totalCount2: {{ totalCount2 }}</div>
+  <div>{{ computedTotalCount }}</div>
+</template>
+
+<style scoped></style>
