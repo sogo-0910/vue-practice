83cea310292140ebbc3f32ff4241fab4d38ba57d
sougo0910 <sougo.ks@gmail.com>
2025-01-09 00:02:10 +0900

    feat:pathの正規表現/404

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import FoodSelectView from '@/views/FoodSelectView.vue'
 import BlogView from '@/views/BlogView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -23,9 +24,18 @@ const router = createRouter({
       component: FoodSelectView,
     },
     {
-      path: '/blog/:id',
+      // default: ([^/]+) /以外の文字全てという意味
+      // +にすると/区切りで配列にしてくれる
+      // ?にするとリアクティブな値がなくても表示される
+      // *にすると+と?の２つの指定ができる
+      path: '/blog/id-:id*',
       name: 'blog',
       component: BlogView,
+    },
+    {
+      path: '/:catchAll(.*)*',
+      name: 'notFound',
+      component: NotFoundView,
     },
   ],
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import TextBase from '@/components/TextBase.vue'
+  import HeadingLevel1 from './../components/HeadingLevel1.vue'
+</script>
+
+<template>
+  <HeadingLevel1 subtitle="404">Not Found</HeadingLevel1>
+  <TextBase>{{ $route.params }}</TextBase>
+</template>
